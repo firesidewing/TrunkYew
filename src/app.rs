ecdbad586fcf12ae99b52a719ecfe89d9fcfd239
Josh Dollinger <32228811+firesidewing@users.noreply.github.com>
2022-09-10 21:55:57 -0700

    tailwindcss

--- src/app.rs
+++ src/app.rs
@@ -3,10 +3,12 @@ use yew::prelude::*;
 #[function_component(App)]
 pub fn app() -> Html {
     html! {
-        <main>
-            <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
-            <h1>{ "Hello World!" }</h1>
-            <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
+        <main class="container mx-auto px-4 h-screen flex">
+            <div class="grid m-auto">
+                <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
+                <h1>{ "Hello World!" }</h1>
+                <span class="subtitle">{ "from Yew with " }</span>
+            </div>
         </main>
     }
 }
